818221bc6ec4016431eeea28e89017f38f3703e9
TylerUnderwood <tylergrantunderwood@gmail.com>
2026-05-31 15:56:29 -0500

    startup code

---
+++ scripts/DCC/Question/2026-05-31.js
@@ -0,0 +1,10 @@
+const currentTest = {
+  label: "getCombinations",
+  tests: [
+    {guess: getCombinations(2), answer: 2},
+    {guess: getCombinations(3), answer: 5},
+    {guess: getCombinations(5), answer: 42},
+    {guess: getCombinations(8), answer: 1430},
+    {guess: getCombinations(13), answer: 742900},
+  ]
+}
--- scripts/DCC/TylerUnderwood/2026-05-31.js
+++ scripts/DCC/TylerUnderwood/2026-05-31.js
@@ -3,14 +3,3 @@ function getCombinations(n) {
 
     return combinationsCount;
 }
-
-const currentTest = {
-  label: "getCombinations",
-  tests: [
-    {guess: getCombinations(2), answer: 2},
-    {guess: getCombinations(3), answer: 5},
-    {guess: getCombinations(5), answer: 42},
-    {guess: getCombinations(8), answer: 1430},
-    {guess: getCombinations(13), answer: 742900},
-  ]
-}
