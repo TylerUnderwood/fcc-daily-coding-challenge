88b8c55349cf443831d0b915e6c65c1db1053e9a
TylerUnderwood <tylergrantunderwood@gmail.com>
2026-05-31 15:39:13 -0500

    startup code

--- scripts/DCC/TylerUnderwood/2026-05-31.js
+++ scripts/DCC/TylerUnderwood/2026-05-31.js
@@ -9,8 +9,8 @@ const currentTest = {
   tests: [
     {guess: getCombinations(2), answer: 2},
     {guess: getCombinations(3), answer: 5},
-    {guess: getCombinations(3), answer: 42},
-    {guess: getCombinations(3), answer: 1430},
-    {guess: getCombinations(3), answer: 742900},
+    {guess: getCombinations(5), answer: 42},
+    {guess: getCombinations(8), answer: 1430},
+    {guess: getCombinations(13), answer: 742900},
   ]
 }
