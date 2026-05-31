1dca147cbca49bd79cf387f0c0d4f73373907f10
TylerUnderwood <tylergrantunderwood@gmail.com>
2026-05-31 15:36:34 -0500

    startup code

---
+++ scripts/DCC/TylerUnderwood/2026-05-31.js
@@ -0,0 +1,16 @@
+function getCombinations(n) {
+    let combinationsCount = 0
+
+    return combinationsCount;
+}
+
+const currentTest = {
+  label: "getCombinations",
+  tests: [
+    {guess: getCombinations(2), answer: 2},
+    {guess: getCombinations(3), answer: 5},
+    {guess: getCombinations(3), answer: 42},
+    {guess: getCombinations(3), answer: 1430},
+    {guess: getCombinations(3), answer: 742900},
+  ]
+}
