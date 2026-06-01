a0fb68b1b61dea78ff7627d455f71b124df37eb4
TylerUnderwood <tylergrantunderwood@gmail.com>
2026-05-31 19:13:55 -0500

    answer

--- scripts/DCC/TylerUnderwood/2026-05-31.js
+++ scripts/DCC/TylerUnderwood/2026-05-31.js
@@ -1,5 +1,25 @@
-function getCombinations(n) {
-    let combinationsCount = 0
+// @see https://www.youtube.com/watch?v=fczN0BCx0xs
 
-    return combinationsCount;
+function getCombinations(n) {
+    if (n <= 0) return 1;
+    let catalan = 1;
+    for (let i = 1; i <= n; i++) {
+        catalan = (catalan * 2 * (2 * i - 1)) / (i + 1);
+    }
+    return catalan;
 }
+
+/**
+NOTES
+
+1. All viable sets will start with open and end with closed
+
+3 Pair === 5 Sets
+Visual
+()()()
+(())()
+()(())
+(()())
+((()))
+
+*/
