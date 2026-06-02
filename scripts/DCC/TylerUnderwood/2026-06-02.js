2a489afd71bebd813ccef55ac96685610821dba5
TylerUnderwood <tylergrantunderwood@gmail.com>
2026-06-02 14:32:45 -0500

    tyler 2026-06-02 answer

--- scripts/DCC/TylerUnderwood/2026-06-02.js
+++ scripts/DCC/TylerUnderwood/2026-06-02.js
@@ -1,4 +1,9 @@
 function isValidSchema(obj) {
-
+  if (!obj.hasOwnProperty('username')) return false
+  if (typeof obj.username !== 'string' ) return false
+  if (!obj.hasOwnProperty('posts')) return false
+  if (typeof obj.posts !== 'number' ) return false
+  if (!obj.hasOwnProperty('verified')) return false
+  if (typeof obj.verified !== 'boolean' ) return false
   return true
 }
