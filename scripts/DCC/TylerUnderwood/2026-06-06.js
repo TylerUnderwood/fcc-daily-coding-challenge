f77bcbf4a1776f64e12703e54c8827d09a1c3bce
TylerUnderwood <tylergrantunderwood@gmail.com>
2026-06-06 19:15:32 -0500

    2026-06-06 setup

---
+++ scripts/DCC/TylerUnderwood/2026-06-06.js
@@ -0,0 +1,19 @@
+// Roles = "user" | "creator" | "moderator" | "staff" | "admin"
+
+// UserProfile = {
+//   username: string,
+//   posts: number,
+//   verified: boolean,
+//   role: Roles,
+//   supporter?: boolean,
+//   badges: string[]
+// }
+
+// {
+//   users: UserProfile[]
+// }
+
+function isValidSchema(obj) {
+
+  return true
+}
