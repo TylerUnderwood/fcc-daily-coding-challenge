bdefef568ca57c9ca3f00bd6cc87fd1e2c736ada
Tyler Underwood <tylergrantunderwood@gmail.com>
2026-06-15 21:13:42 -0500

    fix: update test-logger to handle obj

--- scripts/DCC/index.js
+++ scripts/DCC/index.js
@@ -13,13 +13,22 @@ const testsLogger = (label, tests) => {
   console.group(msgText(`--- ${label} ---`), msgStyle({pad: "5px 12px"}))
 
   tests.forEach((test, index) => {
-    const guess = Array.isArray(test.guess) ? test.guess.toString() : test.guess
-    const answer = Array.isArray(test.answer) ? test.answer.toString() : test.answer
+    let guess, answer;
+
+    [test.guess, test.answer].forEach((val, i) => {
+      if (Array.isArray(val)) {
+        i === 0 ? guess = val.toString() : answer = val.toString()
+      } else if (typeof val === "object") {
+        i === 0 ? guess = JSON.stringify(val) : answer = JSON.stringify(val)
+      } else {
+        i === 0 ? guess = val : answer = val
+      }
+    })
 
     if (guess === answer) {
-      console.log(msgText(`${index}. ${test.guess} CORRECT!`), msgStyle({bg: "225522", color: "55ff55"}))
+      console.log(msgText(`${index}. ${guess} CORRECT!`), msgStyle({bg: "225522", color: "55ff55"}))
     } else {
-      console.log(msgText(`${index}. ${test.guess} Expected ${test.answer}`), msgStyle({bg: "552222", color: "ff5555"}))
+      console.log(msgText(`${index}. ${guess} Expected ${answer}`), msgStyle({bg: "552222", color: "ff5555"}))
     }
   })
 
