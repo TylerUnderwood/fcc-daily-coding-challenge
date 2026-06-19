531b6744e35da065bb1e3628959d772abd370be7
Tyler Underwood <tylergrantunderwood@gmail.com>
2026-06-19 12:19:41 -0500

    chore: add past questions

---
+++ scripts/DCC/TylerUnderwood/2026-06-17.js
@@ -0,0 +1,7 @@
+testsLogger("cast", [
+    { guess: cast("fihwl"), answer: 33 },
+    { guess: cast("lwswfi"), answer: 45 },
+    { guess: cast("wislhfl"), answer: 37 },
+    { guess: cast("sihwlih"), answer: 50 },
+    { guess: cast("wishlfihwslwifihl"), answer: 101 }
+])
