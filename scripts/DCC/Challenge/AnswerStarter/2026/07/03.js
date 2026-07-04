42e04f2676799d55b5ad388309939ef17a559c5b
TylerUnderwood <tylergrantunderwood@gmail.com>
2026-07-04 15:47:37 -0500

    chore add challenge 2026-07-01 through 04

--- scripts/DCC/Challenge/AnswerStarter/2026/07/03.js
+++ scripts/DCC/Challenge/AnswerStarter/2026/07/03.js
@@ -1,7 +1,4 @@
-testsLogger("migrateRecord", [
-    { guess: migrateRecord({ username: "", posts: 0 }, { verified: true }), answer: { username: "", posts: 0, verified: true } },
-    { guess: migrateRecord({ username: "", posts: 0 }, { username: "camper", posts: 5 }), answer: { username: "camper", posts: 5 } },
-    { guess: migrateRecord({ username: "", posts: 0, verified: false }, { username: "camper" }), answer: { username: "camper", posts: 0, verified: false } },
-    { guess: migrateRecord({ username: "", posts: 0 }, { username: "camper", role: "admin" }), answer: { username: "camper", role: "admin", posts: 0 } },
-    { guess: migrateRecord({ username: "", email: "", posts: 0, verified: false, role: "user", banned: false }, { username: "camper", email: "camper@freecodecamp.org", role: "admin" }), answer: { username: "camper", email: "camper@freecodecamp.org", role: "admin", posts: 0, verified: false, banned: false } }
-])
+function migrateRecord(schema, record) {
+
+  return schema;
+}
--- scripts/DCC/Challenge/AnswerStarter/2026/07/04.js
+++ scripts/DCC/Challenge/AnswerStarter/2026/07/04.js
@@ -1,7 +1,4 @@
-testsLogger("kaprekar", [
-    { guess: kaprekar(1234), answer: 3 },
-    { guess: kaprekar(2025), answer: 6 },
-    { guess: kaprekar(7173), answer: 4 },
-    { guess: kaprekar(3164), answer: 7 },
-    { guess: kaprekar(8082), answer: 2 }
-])
+function kaprekar(n) {
+
+  return n;
+}
